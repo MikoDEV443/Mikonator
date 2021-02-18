0c7835590dd720fa9bc1842806dcf6a8c2568cc2
Miko <miko.kankariko@outlook.com>
2021-02-18 19:13:44 +0100

    Upload

---
+++ Mikonator/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mikonator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            if (File.Exists("./icons/pub.cert"))
+            {
+                Myglobals.filenamepk = "./icons/pub.cert";
+                Myglobals.filenamesk = "./icons/priv.cert";
+
+            }
+            else
+            {
+                MessageBox.Show("When you start the Program for the first time you have to run it as admin");
+                System.Environment.Exit(0);
+
+            }
+
+
+
+            RsaFileDemo.LaunchEncryption();
+
+        }
+
+        
+        
+
+        private void btndecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            Myglobals.message2decrypt = txtencrypted.Text;
+            try
+            {
+                RsaFileDemo2.LaunchDecryption();
+                txtdecrypted.Text = Myglobals.decrypt;
+            }
+            catch
+            {
+                MessageBox.Show("CANT DECRYPT STRING !!!");
+            }
+           
+
+
+
+
+        }
+
+        private void btnencrypt_Click(object sender, RoutedEventArgs e)
+        {
+            Myglobals.message2encrypt = txtdecrypted.Text;
+            RsaFileDemo.LaunchEncryption();
+            txtencrypted.Text = Myglobals.encrypt;
+
+        }
+
+        private void btnclear_Click(object sender, RoutedEventArgs e)
+        {
+            txtencrypted.Text = "";
+            txtdecrypted.Text = "";
+
+        }
+
+        private void btncopytoClipboard_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Clipboard.SetText(Myglobals.encrypt);
+
+        }
+
+        private void btncopytoclipboard2_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Clipboard.SetText(Myglobals.decrypt);
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnsk_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.DefaultExt = ".cert";
+            ofd.Title = "Select PrivateKey";
+            ///ofd.Filter = "Pem File (.PEM)|*.PEM";
+            if (ofd.ShowDialog() == true)
+            {
+                Myglobals.filenamesk = ofd.FileName;
+
+            }
+        }
+
+        private void btnpk_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.DefaultExt = ".cert";
+            ofd.Title = "Select Publickey";
+            ///ofd.Filter = "Pem File (.PEM)|*.PEM";
+            if (ofd.ShowDialog() == true)
+            {
+                Myglobals.filenamepk = ofd.FileName;
+
+            }
+        }
+    }
+    
+    public static class Myglobals
+    {
+        public static string filenamesk;
+        public static string filenamepk;
+        public static string decrypt;
+        public static string encrypt;
+        public static string encryp2;
+        public static byte[] encryptbytes;
+        public static byte[] encryptbytes2;
+        public static string message2encrypt = "no";
+        public static string message2decrypt;
+        public static string Username;
+        public static string time;
+        public static string amail;
+        public static int zufall2;
+        public static string PublicKey;
+        public static string a;
+        public static string UserID;
+        public static int checkbox;
+        public static string receive;
+        public static string content;
+        public static string decrypt2;
+        public static string restartcmd;
+    }
+}
+   
+
---
+++ Mikonator/decrypt.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Mikonator
+{
+
+
+    class RsaFileDemo2
+    {
+        public static void LaunchDecryption()
+        {
+
+
+            byte[] encrypted = Convert.FromBase64String(Myglobals.message2decrypt);
+            string publicKey = Myglobals.filenamepk;
+            string privateKey = Myglobals.filenamesk;
+            GenerateKeys(publicKey, privateKey);
+            /// byte[] encrypted = Encrypt(publicKey, Encoding.UTF8.GetBytes(message));
+            byte[] decrypted = Decrypt(privateKey, encrypted);
+
+            /// Console.WriteLine("Original\n\t " + message + "\n");
+            ///Console.WriteLine("Encrypted\n\t" + BitConverter.ToString(encrypted).Replace("-", "") + "\n");
+            ///Console.WriteLine("Decrypted\n\t" + Encoding.UTF8.GetString(decrypted));
+
+            ///Console.ReadLine();
+        }
+
+        private static void GenerateKeys(string publicKeyFile, string privateKeyFile)
+        {
+            using (var rsa = new RSACryptoServiceProvider((int)KeySizes.SIZE_4096))
+            {
+                rsa.PersistKeyInCsp = false;
+                string publicKey = rsa.ToXmlString(false);
+                string privateKey = rsa.ToXmlString(true);
+
+                if (File.Exists(privateKeyFile))
+                    File.Exists(privateKeyFile);
+                else File.WriteAllText(privateKeyFile, privateKey);
+
+                if (File.Exists(publicKeyFile))
+                    File.Exists(publicKeyFile);
+                else File.WriteAllText(publicKeyFile, publicKey);
+
+
+
+
+
+            }
+        }
+
+
+
+
+
+        private static byte[] Decrypt(string privateKeyFile, byte[] encrypted)
+        {
+            byte[] decrypted;
+            using (var rsa = new RSACryptoServiceProvider((int)KeySizes.SIZE_4096))
+            {
+                rsa.PersistKeyInCsp = false;
+                string privateKey = File.ReadAllText(privateKeyFile);
+                rsa.FromXmlString(privateKey);
+                decrypted = rsa.Decrypt(encrypted, true);
+
+            }
+
+            Myglobals.decrypt = Encoding.UTF8.GetString(decrypted);
+         
+
+           
+            return decrypted;
+
+
+
+
+
+        }
+    }
+}
